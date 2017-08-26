2db192daefeff6a52dda09f4e7ea688f02f4ac0a
Dario Vladovic <d.vladimyr@gmail.com>
2017-08-26 15:35:44 +0200

    Adding catch-all express handler
    - pushing dist folder to git
    - adding clean script

--- server/index.js
+++ server/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
-const path = require('path');
+const { join } = require('path');
 const { port = process.env.PORT } = require('../package.json').config;
 
 const app = express();
 app.use('/api/calendar', require('./calendar'));
-app.use('/', express.static(path.join(__dirname, '../dist')));
+app.use('/', express.static(join(__dirname, '../dist')));
+app.use('*', (_, res) => res.sendFile(join(__dirname, '../dist/index.html')));
+
 app.listen(port, () => console.log(`Server listening on port ${port}`));
